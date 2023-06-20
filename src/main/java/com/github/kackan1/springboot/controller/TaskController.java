85c6c40eb81044026a3db5966202b3af97960446
Kackan1 <Kacper.Kaniuka@wp.pl>
2023-06-20 17:58:44 +0200

    Added custom rest repository

---
+++ src/main/java/com/github/kackan1/springboot/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.github.kackan1.springboot.controller;
+
+import com.github.kackan1.springboot.model.Task;
+import com.github.kackan1.springboot.model.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RepositoryRestController
+class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    TaskController(final TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort","!page","!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Exposing all the tasks");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+}
