331a7c2dddd15a4c32c4136e9be0cda368316112
Kackan1 <Kacper.Kaniuka@wp.pl>
2023-07-10 12:24:11 +0200

    Database migration with flyway

--- src/main/java/com/github/kackan1/springboot/model/SqlTaskRepository.java
+++ src/main/java/com/github/kackan1/springboot/model/SqlTaskRepository.java
@@ -1,9 +1,14 @@
 package com.github.kackan1.springboot.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+    @Override
+    @Query(nativeQuery = true, value = "SELECT count(*) > 0 FROM tasks WHERE id=:id")
+    boolean existsById(@Param("id") Integer id);
 }
