5913fc4bceee9658338a6ff34f60834ab7387db8
Kackan1 <Kacper.Kaniuka@wp.pl>
2023-11-06 13:21:57 +0100

    Hibernate modeling

--- src/main/java/com/github/kackan1/springboot/model/Task.java
+++ src/main/java/com/github/kackan1/springboot/model/Task.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-public class Task {
+public class Task{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -14,9 +14,8 @@ public class Task {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
-    private LocalDateTime createdOn;
-    private LocalDateTime updatedOn;
-
+    @Embedded
+    private Audit audit = new Audit();
 
     public Task() {
     }
@@ -59,14 +58,6 @@ public class Task {
         deadline = source.deadline;
     }
 
-    @PrePersist
-    void prePersist() {
-        createdOn = LocalDateTime.now();
-    }
 
-    @PreUpdate
-    void preUpdate() {
-        updatedOn = LocalDateTime.now();
-    }
 
 }
