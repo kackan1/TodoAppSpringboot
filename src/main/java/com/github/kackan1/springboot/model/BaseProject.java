2404c84c475b1d75ee3b44d32fad4489c5ba629f
Kackan1 <Kacper.Kaniuka@wp.pl>
2023-11-14 16:53:12 +0100

    feat: Project entity with steps
    Added migration for two new tables
    
    Added their respective entities in model

---
+++ src/main/java/com/github/kackan1/springboot/model/BaseProject.java
@@ -0,0 +1,32 @@
+package com.github.kackan1.springboot.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+
+@MappedSuperclass
+public abstract class BaseProject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Description must be not empty")
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/github/kackan1/springboot/model/Project.java
@@ -0,0 +1,12 @@
+package com.github.kackan1.springboot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "projects")
+public class Project extends BaseProject{
+
+
+
+}
