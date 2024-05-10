5436360b885b521665e716d1c6fcaec8991d59a2
Kackan1 <Kacper.Kaniuka@wp.pl>
2024-05-10 16:57:34 +0200

    Rozwiązany problem zwracania podwójnych projektów przez kontroler

--- src/main/java/com/github/kackan1/springboot/controller/ProjectController.java
+++ src/main/java/com/github/kackan1/springboot/controller/ProjectController.java
@@ -1,14 +1,17 @@
 package com.github.kackan1.springboot.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.github.kackan1.springboot.logic.ProjectService;
@@ -26,7 +29,7 @@ public class ProjectController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping()
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
         return "projects";
@@ -42,6 +45,7 @@ public class ProjectController {
         }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("projects", getProjects());
         model.addAttribute("message", "Dodano projekt!");
         return "projects";
     }
@@ -49,6 +53,21 @@ public class ProjectController {
     @PostMapping(params = "addStep")
     public String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    public String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline) {
+        try {
+            service.createGroup(id, deadline);
+            model.addAttribute("message", "Dodano grupe!");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy");
+        }
         return "projects";
     }
 
